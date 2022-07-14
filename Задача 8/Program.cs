81e8b604ee0d31dbb822c661962440498bea3a2f
Игорь Наточий <igornatochiy96@mail.ru>
2022-07-15 01:32:05 +0600

    Добавьте файлы проекта.

---
+++ Задача 8/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\Кино";
+            string[] files = Directory.GetFiles(path,"* ", SearchOption.AllDirectories);
+            foreach(string file in files)
+            {
+                Console.WriteLine(file);
+            }
+            Console.ReadKey();
+
+        }
+    }
+}
---
+++ Задача2/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = @"E:\Кино\Numbers.txt";
+            Random random = new Random();
+            using (StreamWriter sw = new StreamWriter(path,false))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    sw.WriteLine(random.Next(-10, 10));
+                }
+            } int sum = 0;
+            using (StreamReader sr = new StreamReader(path))
+            {
+                for (int i = 0; i < 10; i++)
+                {
+                    Convert.ToInt32(sr.ReadLine());
+                    Console.WriteLine(sum);
+                    Console.ReadKey();
+                }
+            }
+        }
+    }
+}
